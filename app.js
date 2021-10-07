93a209aa46cbbe694491299a2d47a25c49714c9a
WagnerMor <wagnermor@outlook.com>
2021-10-07 01:21:05 -0300

    added app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const retangulo = require('./modulos/ex01-retangulo');
+const quadrado = require('./modulos/ex02-quadrado');
+const circulo = require('./modulos/ex03-circulo');
+const triangulo = require('./modulos/ex04-triangulo');
+
+// console.log(triangulo(4).area);
+console.log(triangulo(5,10,20).perimetro);
+console.log(triangulo().error);
