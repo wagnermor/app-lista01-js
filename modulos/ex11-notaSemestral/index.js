020f4ce1326da0d2b48e53b9ec58ed0304fa55c9
WagnerMor <wagnermor@outlook.com>
2021-10-08 23:58:12 -0300

    added ex11 module

---
+++ modulos/ex11-notaSemestral/index.js
@@ -0,0 +1,4 @@
+//11. Faça um algoritmo para calcular a nota semestral de um aluno. A nota semestral é obtida pela média aritmética entre a nota de 2 bimestres. Cada nota de bimestre é composta por 2 notas de provas.
+const mediaSemestral = (nota1Bimestre, nota2Bimestre) => (nota1Bimestre + nota2Bimestre) / 2;
+
+module.exports = mediaSemestral;
