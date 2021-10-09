405ac79493fcdfe6303e68a00c3193315347dd78
WagnerMor <wagnermor@outlook.com>
2021-10-08 23:58:50 -0300

    added ex12

---
+++ modulos/ex12-velocidade/index.js
@@ -0,0 +1,4 @@
+//12. Faça um algoritmo que transforme uma velocidade fornecida em m/s pelo usuário para Km/h. Para tal, multiplique o valor em m/s por 3,6.
+const msToKh = (metrosSegundo) => `${metrosSegundo} m/s = ${metrosSegundo * 3.6} k/h`;
+
+module.exports = msToKh;
