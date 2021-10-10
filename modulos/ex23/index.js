f66e10537ee0f6b7a4f2d60c9a16c5c1ed0e96db
WagnerMor <wagnermor@outlook.com>
2021-10-10 01:21:18 -0300

    added ex23 module

---
+++ modulos/ex23/index.js
@@ -0,0 +1,4 @@
+//23. Escreva um programa que receba um número e imprima se este número é ou não par.
+const parOuImpar = (numero) => numero % 2 === 0 ? "par".toUpperCase() : "impar".toUpperCase();
+
+module.exports = parOuImpar;
