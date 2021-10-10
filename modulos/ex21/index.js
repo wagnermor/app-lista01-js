7d97cd271477dedb091b540cc324bdf0572806f2
WagnerMor <wagnermor@outlook.com>
2021-10-10 01:05:38 -0300

    added ex21 module

---
+++ modulos/ex21/index.js
@@ -0,0 +1,4 @@
+//21. Escreva um programa que receba um número e exiba se ele é positivo ou negativo.
+const positivoOuNegativo = (numero) => numero > 0 ? `O número ${numero} é ${'positivo'.toUpperCase()}` : `O número ${numero} é ${'negativo'.toUpperCase()}`;
+
+module.exports =positivoOuNegativo;
