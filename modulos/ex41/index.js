23d64635dfeb99c8c2ecaa082a692199214203a9
WagnerMor <wagnermor@outlook.com>
2021-10-10 23:26:33 -0300

    added ex41 module

---
+++ modulos/ex41/index.js
@@ -0,0 +1,6 @@
+//41. Escreva um programa que imprima todos os números inteiros do intervalo fechado de 1 a 100.
+const numeroCrescente = () => {
+  for (let i = 1; i <= 100; i += 1) console.log(`--> ${i}`);
+}
+
+module.exports = numeroCrescente;
