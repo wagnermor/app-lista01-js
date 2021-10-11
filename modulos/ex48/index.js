2619227a4313be439d27161337a22f266650632e
WagnerMor <wagnermor@outlook.com>
2021-10-10 23:27:20 -0300

    added ex48 module

---
+++ modulos/ex48/index.js
@@ -0,0 +1,14 @@
+//48. Escreva um programa que determine se um dado número N  é primo ou não.
+const numeroPrimo = (numero) => {
+  for (let i = 2; i < numero; i += 1) {
+    if (numero % i === 0) {
+      return false;
+    }
+    // return numero > 1;
+    return true;
+  }
+}
+
+const imprimeNumeroPrimo = (n) => numeroPrimo(n) ? `O nº ${n} é primo` : `O nº ${n} NÃO é primo`;
+
+module.exports = imprimeNumeroPrimo;
